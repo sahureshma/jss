dd4769ef7f8705f26aa9a431692f0fd95de5c0e1
sahureshma <ssshureshma17299@gmail.com>
2023-04-26 18:35:18 +0530

    count value

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import{useState} from 'react'
 
 function App() {
+  const[count,setCount]=useState(0)
+  function handclick(){
+    const newCount=count+1
+    setCount(newCount)
+
+  }
+  function handleclick(){
+    
+    const newCount=count-1
+    setCount(newCount)
+
+   
+       
+     
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h4>count is:</h4>
+      <h4>{count}</h4>
+      <h1>{count}</h1>
+      <button onClick={handclick}>Increase the count</button>
+      <button onClick={handleclick}>Decrease the count</button>
+  
+      
     </div>
   );
 }
