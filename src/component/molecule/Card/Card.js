d20f96d59232115c45f99fbe6c201a7076d2ada9
sahureshma <ssshureshma17299@gmail.com>
2023-05-10 19:49:40 +0530

    scrollbar card

---
+++ src/component/molecule/Card/Card.js
@@ -0,0 +1,123 @@
+import styles from './Card.module.css'
+
+export default function Card () {
+    
+
+
+    return (
+        <div className={styles.wrapper}>
+            <div className={styles.card}>
+            <img
+                src='https://images.pexels.com/photos/3184611/pexels-photo-3184611.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+                height = '100px'
+                width = '100px'
+                style={{borderRadius : '50%'}}
+               
+            />
+            <h4>Rahul verma</h4>
+            <p>Mentor</p>
+           
+            </div>
+            <div className={styles.card}>
+            <img
+                src='https://images.pexels.com/photos/716411/pexels-photo-716411.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+                height = '100px'
+                width = '100px'
+                style={{borderRadius : '50%'}}
+            />
+            <h4>Vicky Roy</h4>
+            <p>Software Developer</p>
+           
+            </div>
+            <div className={styles.card}>
+            <img
+                src='https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+                height = '100px'
+                width = '100px'
+                style={{borderRadius : '50%'}}
+            />
+            <h4>Ram Dube</h4>
+            <p>Manager</p>
+         
+            </div>
+            <div className={styles.card}>
+            <img
+                src='https://images.pexels.com/photos/32976/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+                height = '100px'
+                width = '100px'
+                style={{borderRadius : '50%'}}
+            />
+            <h4>Ramkapoor</h4>
+            <p>Software engineer</p>
+            </div>
+            <div className={styles.card}>
+            <img
+                src='https://images.pexels.com/photos/927451/pexels-photo-927451.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+                height = '100px'
+                width = '100px'
+                style={{borderRadius : '50%'}}
+            />
+            <h4>Gopal Mishra</h4>
+            <p>Mentor</p>
+            </div>
+            <div className={styles.card}>
+            <img
+                src='https://images.pexels.com/photos/842548/pexels-photo-842548.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+                height = '100px'
+                width = '100px'
+                style={{borderRadius : '50%'}}
+            />
+            <h4>Ritik khan</h4>
+            <p>Developer</p>
+            
+            </div>
+            <div className={styles.card}>
+            <img
+                src='https://images.pexels.com/photos/3778876/pexels-photo-3778876.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+                height = '100px'
+                width = '100px'
+                style={{borderRadius : '50%'}}
+            />
+            <h4>Rohit Kumar</h4>
+            <p>Developer</p>
+            
+            </div>
+            <div className={styles.card}>
+            <img
+                src='https://images.pexels.com/photos/1043473/pexels-photo-1043473.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+                height = '100px'
+                width = '100px'
+                style={{borderRadius : '50%'}}
+            />
+            <h4>Sakti Sahu</h4>
+            <p>Manager</p>
+            
+            </div>
+           < div className={styles.card}>
+            <img
+                src='https://images.pexels.com/photos/16589636/pexels-photo-16589636.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+                height = '100px'
+                width = '100px'
+                style={{borderRadius : '50%'}}
+            />
+            <h4>Luana Freitas</h4>
+            <p>SDE</p>
+            
+            </div>
+            < div className={styles.card}>
+            <img
+                src='https://images.pexels.com/photos/16578958/pexels-photo-16578958.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+                height = '100px'
+                width = '100px'
+                style={{borderRadius : '50%'}}
+            />
+            <h4>Rani Mishra</h4>
+            <p>SDE</p>
+            
+            </div>
+           
+           
+                
+        </div>
+    )
+}
